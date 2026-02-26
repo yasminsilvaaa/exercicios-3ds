5b4f07b283974311c2d23e35ece4078d6a9aabc2
yasmin silva <yasmin.nascimento50@aluno.ce.gov.br>
2026-02-26 12:48:08 +0000

    projeto finalizado2

--- 1-bimestre/teste-de-software/projetos/projeto1-sistema-de-validacao-de-pedidos/index.js
+++ 1-bimestre/teste-de-software/projetos/projeto1-sistema-de-validacao-de-pedidos/index.js
@@ -1,15 +1,17 @@
-function validarPedido(pedido) {
-  if (!pedido) return false;
-
-  if (!pedido.cliente) return false;
-
-  if (pedido.total <= 0) return false;
-
-  if (!Array.isArray(pedido.itens) || pedido.itens.length === 0) {
-    return false;
+function validarPedido(pedido){
+    if (!pedido.cliente) {
+    return false
   }
 
-  return true;
+  if (pedido.total <= 0) {
+    return false
+  }
+
+  if (!pedido.itens || pedido.itens.length === 0) {
+    return false
+  }
+
+  return true
 }
 
-module.exports = validarPedido;
+module.exports = validarPedido
--- 1-bimestre/teste-de-software/projetos/projeto1-sistema-de-validacao-de-pedidos/test/index.test.js
+++ 1-bimestre/teste-de-software/projetos/projeto1-sistema-de-validacao-de-pedidos/test/index.test.js
@@ -1,53 +1,36 @@
-const validarPedido = require('../index');
+const validarPedido = require('../index')
 
-describe('Sistema de Validação de Pedido', () => {
-
-  test('Pedido válido deve retornar true', () => {
+test("Pedido válido deve retornar true", () => {
     const pedido = {
-      cliente: "Carlos",
-      total: 150,
-      itens: ["Produto A", "Produto B"]
-    };
+        cliente: "Carlos",
+        total: 150,
+        itens: ["Produto A"],
+    }
+     expect(validarPedido(pedido)).toBe(true)
+})
 
-    expect(validarPedido(pedido)).toBe(true);
-  });
+test("Pedido com total 0 deve retornar false", () =>{
+    const pedido ={
+       cliente: "Carlos",
+	   total: 0,
+	   itens: ["Produto A"],
+    }
+    expect(validarPedido(pedido)).toBe(false)
+})
 
-  test('Pedido com total 0 deve retornar false', () => {
-    const pedido = {
-      cliente: "Carlos",
-      total: 0,
-      itens: ["Produto A"]
-    };
+test("Pedido sem cliente deve retornar false", () => {
+    const pedido ={
+       cliente: "",
+	   total: 150,
+	   itens: ["Produto A"],
+    }
+ expect(validarPedido(pedido)).toBe(false)
+})
 
-    expect(validarPedido(pedido)).toBe(false);
-  });
-
-  test('Pedido sem cliente deve retornar false', () => {
-    const pedido = {
-      total: 150,
-      itens: ["Produto A"]
-    };
-
-    expect(validarPedido(pedido)).toBe(false);
-  });
-
-  test('Pedido sem itens deve retornar false', () => {
-    const pedido = {
-      cliente: "Carlos",
-      total: 150
-    };
-
-    expect(validarPedido(pedido)).toBe(false);
-  });
-
-  test('Pedido com itens vazio deve retornar false', () => {
-    const pedido = {
-      cliente: "Carlos",
-      total: 150,
-      itens: []
-    };
-
-    expect(validarPedido(pedido)).toBe(false);
-  });
-
-});
+test("Pedido sem itens deve retornar false", () => {
+    const pedido ={
+       cliente: "Carlos",
+	   total: 150,
+	   itens: [""],
+    }
+}) 
